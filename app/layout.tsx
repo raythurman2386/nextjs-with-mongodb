761bbdd51216db22caafc4cc3cbc0de5ee4cd635
rthurman2386 <raymondthurman5@gmail.com>
2024-03-14 22:55:31 -0500

    Prettier installation, configuration formatting and workflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,20 @@ import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Home",
-  description: "Welcome to Next.js with MongoDB!",
+    title: "Home",
+    description: "Welcome to Next.js with MongoDB!",
 };
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,36 +5,36 @@ const MONGODB_URI = process.env.MONGODB_URI as string;
 const MONGODB_DB = process.env.MONGODB_DB as string;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
+    throw new Error("Please define the MONGODB_URI environment variable");
 }
 
 if (!MONGODB_DB) {
-  throw new Error("Please define the MONGODB_DB environment variable");
+    throw new Error("Please define the MONGODB_DB environment variable");
 }
 
 interface CachedConnection {
-  client: MongoClient;
-  db: Db;
+    client: MongoClient;
+    db: Db;
 }
 
 let cachedConnection: CachedConnection | null = null;
 
 export async function connectToDatabase(): Promise<CachedConnection> {
-  if (cachedConnection) {
+    if (cachedConnection) {
+        return cachedConnection;
+    }
+
+    const client = await MongoClient.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const db = client.db(MONGODB_DB);
+
+    cachedConnection = {
+        client,
+        db,
+    };
+
     return cachedConnection;
-  }
-
-  const client = await MongoClient.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  const db = client.db(MONGODB_DB);
-
-  cachedConnection = {
-    client,
-    db,
-  };
-
-  return cachedConnection;
 }
